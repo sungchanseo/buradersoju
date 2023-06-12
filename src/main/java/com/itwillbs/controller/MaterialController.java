1cfd8b7e339aad2d28078485362cfe71bb4e1d33
LingLing23 <ryoung6920@gmail.com>
2023-06-12 17:28:32 +0900

    06/12 17:28 + 구매팀 기본 큰 틀 작성

---
+++ src/main/java/com/itwillbs/controller/MaterialController.java
@@ -0,0 +1,15 @@
+package com.itwillbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping(value = "/material/*")
+public class MaterialController {
+
+	// http://localhost:8088/material/list.jsp
+	
+	
+}

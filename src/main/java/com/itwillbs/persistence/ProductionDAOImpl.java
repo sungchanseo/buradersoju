78fa384eaf361318f3f32e904ec7396008666839
hyyso <hysoo5223@gmail.com>
2023-06-18 22:07:35 +0900

    06-18 22:07 머지 충돌생긴 파일 중복부분 삭제

--- src/main/java/com/itwillbs/persistence/ProductionDAOImpl.java
+++ src/main/java/com/itwillbs/persistence/ProductionDAOImpl.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
@@ -17,30 +18,6 @@ import com.itwillbs.domain.ProductionVO;
 @Repository
 public class ProductionDAOImpl implements ProductionDAO {
 	
-	// 디비연결정보, mapper접근 => sqlSession 객체
-	@Autowired
-	private SqlSession sqlSession;
-	
-	private static final Logger logger = LoggerFactory.getLogger(ProductionController.class);
-	
-	// mapper의 namespace 정보
-	private static final String NAMESPACE = "com.itwillbs.mapper.productionMapper";
-
-	
-	@Override
-	public void insertWorkOrder(ProductionVO vo) throws Exception {
-		logger.debug(" sqlSession - Mabatis - mapper - DB ");
-		int result = sqlSession.insert(NAMESPACE+".insert", vo);
-		
-		if(result != 0)
-			logger.debug(" 글쓰기 완료! ");
-	}
-
-	@Override
-	public List<ProductionVO> getWorkOrderList() {
-		
-		return sqlSession.selectList(NAMESPACE+".getWorkOrderList");
-	}
 
 	// DB 연결 (의존주입)
 	@Inject

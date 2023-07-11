0b0383fe51f8c8f73e3d581f4278a19a8ed30f19
LCHova <lch1014l@naver.com>
2023-07-11 12:50:28 +0900

    Fix    :     공지, 로그인 수정

--- src/main/java/com/itwillbs/controller/LoginController.java
+++ src/main/java/com/itwillbs/controller/LoginController.java
@@ -18,13 +18,11 @@ import com.itwillbs.service.LoginService;
 @Controller
 @RequestMapping(value = "/main/*")
 public class LoginController {
-	//3-1. 서비스 처리 객체를 주입(DI)
 	@Inject
 	private LoginService service;
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
-	/* 로그인 기능 */
-	// http://localhost:8088/main/login
+	//로그인
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginGET() throws Exception{
 		logger.info("C: 로그인 입력페이지 GET");
@@ -34,36 +32,30 @@ public class LoginController {
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String loginPOST(LoginVO vo, HttpSession session, Model model, RedirectAttributes rttr) throws Exception{
 		logger.info("C: 로그인 처리페이지 POST");
-		logger.info("C: vo" + vo );
-		//1.한글처리 => web.xml에서 처리 완료
-		//2.전달받은 파라미터 저장 => loginPOST()메서드의 파라미터값으로 저장함.
-		logger.info("C: 로그인 처리"+ vo.getEmp_id() +", "+ vo.getEmp_pw());
-		//3.서비스객체생성 =>  의존주입완료
-		//4.서비스 로그인 체크 동작(HttpSession)
+//		logger.info("C: vo" + vo );
+//		logger.info("C: 로그인 처리"+ vo.getEmp_id() +", "+ vo.getEmp_pw());
+
 		LoginVO returnVO = service.loginMember(vo);
-		logger.info("C: 리턴VO결과(서비스에서 예외처리를 진행했으므로 null이 출력되면 코드에 문제있다는 의미) "+returnVO);
+//		logger.info("C: 리턴VO : "+returnVO);
 		
-			//5.메인페이지로 이동(주소줄과 view페이지 동시에 main으로 변경되어야함)
-			// 해당 정보 있는 경우 : => main페이지로 이동
 		if(returnVO != null) {
-			//5.세션값생성
 			session.setAttribute("emp_id", returnVO.getEmp_id());
 			session.setAttribute("emp_name", returnVO.getEmp_name());
 			session.setAttribute("emp_department", returnVO.getEmp_department());
 			session.setAttribute("emp_image", returnVO.getEmp_image());
 			rttr.addFlashAttribute("mvo", returnVO);
-			//l.info("C: 모델값 : "+model);
+
 			logger.info("로그인 성공!");
 			 return "redirect:/main";
 		}else {
-			 // 해당 정보 없는 경우: 로그인 실패
+			 // 정보가 없는 경우
 		    logger.info("로그인 실패!");
 		    model.addAttribute("loginFailed", true);
 		    return "main/loginError";
 		}
-	}//end of loginPOST()
+	}
 	
-	// 로그아웃
+	//로그아웃
 	@RequestMapping(value = "/logout",method = RequestMethod.GET)
 	public String logoutGET(HttpSession session) {
 		logger.debug("logoutGET() 호출!");
@@ -71,7 +63,6 @@ public class LoginController {
 		// 세션정보 초기화
 		session.invalidate();
 		
-		//return "redirect:/member/login";
 		return "redirect:/main/login";
 	}
 
